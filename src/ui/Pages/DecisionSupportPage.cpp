ab697dafa1eccf0356fee411d498b6a591cc6090
alviansm <sandalproject7@gmail.com>
2025-09-24 18:33:15 +0700

    add: layout widgets for decision support page

--- src/ui/Pages/DecisionSupportPage.cpp
+++ src/ui/Pages/DecisionSupportPage.cpp
@@ -4,11 +4,120 @@
 DecisionSupportPage::DecisionSupportPage(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::DecisionSupportPage)
+    , m_splitter(nullptr)
+    , m_sidebarContainer(nullptr)
+    , m_topSidebar(nullptr)
+    , m_mainSidebar(nullptr)
+    , m_contentContainer(nullptr)
+    , m_topContent(nullptr)
+    , m_mainContent(nullptr)
+    , m_currentPage(CIIPerformanceComparisonGraph)
 {
     ui->setupUi(this);
+
+    setupWidget();
+    createPageContent();
+    setupStylesheet();
+
+    // Set initial page
+    setCurrentPage(CIIPerformanceComparisonGraph);
 }
 
 DecisionSupportPage::~DecisionSupportPage()
 {
     delete ui;
+}
+
+void DecisionSupportPage::setCurrentPage(int pageIndex)
+{
+    if (pageIndex >= 0 && pageIndex < 5) // We have 5 pages
+    {
+        m_currentPage = pageIndex;
+
+        // Update all the main content stacked widget to show the same page index
+        m_mainContent->setCurrentIndex(pageIndex);
+
+        qInfo() << "Current page changed to:" << pageIndex;
+    }
+}
+
+void DecisionSupportPage::createPageContent()
+{
+
+}
+
+void DecisionSupportPage::createPageWidgets(int pageIndex, const QString &pageName)
+{
+
+}
+
+void DecisionSupportPage::setupWidget()
+{
+    // --- Splitter ---
+    m_splitter = new QSplitter(this);
+    m_splitter->setOrientation(Qt::Horizontal);
+
+    // --- Sidebar container ---
+    QWidget *sidebarWidget = new QWidget(m_splitter);
+    m_sidebarContainer = new QVBoxLayout(sidebarWidget);
+    m_sidebarContainer->setContentsMargins(0, 0, 0, 0);
+    m_sidebarContainer->setSpacing(6);
+
+    // Create stacked widgets instead of regular widgets
+    m_topSidebar = new QStackedWidget(sidebarWidget);
+    m_mainSidebar = new QStackedWidget(sidebarWidget);
+
+    // Ratio 1:3 vertically in the sidebar
+    m_sidebarContainer->addWidget(m_topSidebar, 1);  // stretch factor 1
+    m_sidebarContainer->addWidget(m_mainSidebar, 3); // stretch factor 3
+
+    // --- Content container ---
+    QWidget *contentWidget = new QWidget(m_splitter);
+    m_contentContainer = new QVBoxLayout(contentWidget);
+    m_contentContainer->setContentsMargins(0, 0, 0, 0);
+    m_contentContainer->setSpacing(6);
+
+    // Create stacked widgets instead of regular widgets
+    m_topContent = new QStackedWidget(contentWidget);
+    m_mainContent = new QStackedWidget(contentWidget);
+
+    // Ratio 1:3 vertically in the content
+    m_contentContainer->addWidget(m_topContent, 1);  // stretch factor 1
+    m_contentContainer->addWidget(m_mainContent, 3); // stretch factor 3
+
+    // --- Add to splitter ---
+    // Sidebar first, content second
+    m_splitter->addWidget(sidebarWidget);
+    m_splitter->addWidget(contentWidget);
+
+    // --- Sidebar sizing rules ---
+    sidebarWidget->setMinimumWidth(240);
+    sidebarWidget->setMaximumWidth(280);
+
+    // Allow splitter to resize content dynamically
+    QList<int> sizes;
+    sizes << 260 << 700; // initial sizes, sidebar ~260px
+    m_splitter->setSizes(sizes);
+
+    // --- Top-level layout ---
+    QVBoxLayout *mainLayout = new QVBoxLayout(this);
+    mainLayout->setContentsMargins(0, 0, 0, 0);
+    mainLayout->setSpacing(0);
+    mainLayout->addWidget(m_splitter);
+    this->setLayout(mainLayout);
+}
+
+void DecisionSupportPage::setupStylesheet()
+{
+    QString basicStyleSheet = "QWidget { background-color: #212121; }";
+    m_topSidebar->setStyleSheet(basicStyleSheet );
+    m_mainSidebar->setStyleSheet(basicStyleSheet );
+    m_topContent->setStyleSheet(basicStyleSheet );
+
+    QString stackedWidgetStyleSheet = "QStackedWidget { background-color: #212121; }";
+    m_mainContent->setStyleSheet(stackedWidgetStyleSheet);
+
+    m_splitter->setStyleSheet(
+        "QSplitter::handle { background: transparent; }"
+    );
 }
--- src/ui/Pages/DecisionSupportPage.h
+++ src/ui/Pages/DecisionSupportPage.h
@@ -2,6 +2,9 @@
 #define DECISIONSUPPORTPAGE_H
 
 #include <QWidget>
+#include <QVBoxLayout>
+#include <QSplitter>
+#include <QStackedWidget>
 
 namespace Ui {
 class DecisionSupportPage;
@@ -15,8 +18,39 @@ public:
     explicit DecisionSupportPage(QWidget *parent = nullptr);
     ~DecisionSupportPage();
 
+    enum MainContentPage {
+        CIIPerformanceComparisonGraph,
+        ThreeYearImplementationSummary,
+        SelfEvaluationFeedbackLoop,
+        CorrectiveActionPlan
+    };
+
+public slots:
+    void setCurrentPage(int pageIndex);
+
+private:
+    void setupWidget();
+    void setupStylesheet();
+    void createPageContent();
+    void createPageWidgets(int pageIndex, const QString& pageName);
+
 private:
     Ui::DecisionSupportPage *ui;
+
+    // Layout widget
+    QSplitter* m_splitter;
+
+    QVBoxLayout* m_sidebarContainer;
+    QWidget* m_topSidebar;
+    QWidget* m_mainSidebar;
+
+    QVBoxLayout* m_contentContainer;
+    QWidget* m_topContent;
+    QStackedWidget* m_mainContent;
+
+    int m_currentPage;
 };
+
+
 
 #endif // DECISIONSUPPORTPAGE_H
